c4912981bfd86a76e728e7f0d942b11cb9097d15
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2021-04-09 10:43:08 -0500

    drop unnecessary code

--- src/cmd/bench-swiper/main.cpp
+++ src/cmd/bench-swiper/main.cpp
@@ -17,10 +17,6 @@
 
 #include "swiper/swiper.hpp"
 
-static void usage(const char* program) {
-    std::cerr << "Usage: " << program << " <hash>" << std::endl;
-}
-
 static void spin(volatile uint_fast32_t n) noexcept {
     while (n-- != 0) {}
 }
@@ -31,12 +27,17 @@ static void warm_cache(char* password, size_t hash_len, const char* hash, volati
     }
 }
 
-int main(int argc, const char** argv) {
-    if (argc < 2) {
-        usage(argv[0]);
-        exit(EXIT_FAILURE);
-    }
-
+/**
+ * @brief main is the entrypoint.
+ *
+ * Usage: bench-swiper <Cisco type 7 hash>
+ *
+ * @param argc argument count
+ * @param argv CLI arguments
+ *
+ * @returns CLI exit code
+ */
+int main(int argc __attribute__((unused)), const char** argv) {
     #if defined(_WIN32)
     ::SetProcessAffinityMask(GetCurrentProcess(), 0x00);
     #elif defined(__linux__)
@@ -47,13 +48,7 @@ int main(int argc, const char** argv) {
     #endif
 
     const auto hash = argv[1];
-
     auto hash_len = strlen(hash);
-
-    if (hash_len > 24) {
-        hash_len = 24;
-    }
-
     char password[12];
     constexpr auto trials = uint_fast32_t(1 << 30);
     const auto nop_start = std::chrono::steady_clock::now();
