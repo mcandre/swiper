34f56fb4c9654a7a40cc7cd5868908b136352bd7
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2021-04-11 21:37:54 -0500

    reduce operations

--- src/swiper.cpp
+++ src/swiper.cpp
@@ -15,7 +15,7 @@ namespace swiper {
          */
         template<class T>
         auto ParseDecPair(const T* pair) noexcept {
-            return 10 * (pair[0] - 48) + (pair[1] - 48);
+            return 10 * pair[0] + pair[1] - 528;
         }
 
         /**
@@ -27,22 +27,21 @@ namespace swiper {
          */
         template<class T>
         auto ParseHexPair(const T* pair) noexcept {
-            return 16 * (pair[0] - (pair[0] & 64 ? 55 : 48)) +
+            return 16 * (pair[0] + (pair[0] & 64 ? 9 : 0)) +
                 pair[1] - (pair[1] & 64 ? 55 : 48);
         }
 
         /**
          * @brief Xlat is a fixed XOR key.
          */
-        constexpr char Xlat[32] = {
+        constexpr char Xlat[27] __attribute__((aligned (16))) = {
             0x64, 0x73, 0x66, 0x64,
             0x3b, 0x6b, 0x66, 0x6f,
             0x41, 0x2c, 0x2e, 0x69,
             0x79, 0x65, 0x77, 0x72,
             0x6b, 0x6c, 0x64, 0x4a,
             0x4b, 0x44, 0x48, 0x53,
-            0x55, 0x42, 0x73, 0x00,
-            0x00, 0x00, 0x00, 0x00
+            0x55, 0x42, 0x73
         };
     }
 
