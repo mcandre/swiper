a234b6fe0b04c2c068b27defbdc8a3afbf1c0435
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2021-04-21 21:04:02 +0000

    clean docs

--- include/swiper/swiper.hpp
+++ include/swiper/swiper.hpp
@@ -19,8 +19,7 @@ namespace swiper {
      *
      * @param password out buffer, min hash_len / 2 - 2 characters, non-null terminated
      * @param hash_len string length of hash
-     * @param hash Cisco Itype 7 hash, uppercase, min length 4
-     *
+     * @param hash Cisco type 7 hash, uppercase, min length 4
      */
     void Decrypt(uint8_t* password, size_t hash_len, const uint8_t* hash) noexcept;
 }
