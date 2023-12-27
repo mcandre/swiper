7c28c40ca2d8b47fca8a2833502e41be046ec0d9
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2023-12-27 14:34:40 -0600

    update karp

--- install.mk
+++ install.mk
@@ -5,7 +5,7 @@
 all:
 	cargo install --force unmake@0.0.12
 
-	go install github.com/mcandre/karp/cmd/karp@v0.0.7
+	go install github.com/mcandre/karp/cmd/karp@v0.0.8
 	go mod tidy
 
 	pip3 install --upgrade pip setuptools
